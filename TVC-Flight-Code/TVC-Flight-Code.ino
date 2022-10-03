40493a5eafd6d5cdac44a6d38c0a6f509ddc18ec
Tom Kuttler <tomkuttler@gmail.com>
2022-10-03 11:05:41 +0200

    Version 1.0 used on first test flight

--- TVC-Flight-Code/TVC-Flight-Code.ino
+++ TVC-Flight-Code/TVC-Flight-Code.ino
@@ -4,7 +4,9 @@
  * This is the source code for a software implementation of thrust vector control (TVC) for model rockets. 
  * The software consists of a state machine and PID control loops to adjust the orientation of the rocket during its powered ascent.
  * 
- * @version 0.9
+ * This version 1.0 was used for the first test flight of the rocket on Sep 03 2022.
+ * 
+ * @version 1.0
  * @author Tom Kuttler
  * @GitHub https://github.com/tomkuttler/TVC-Rocket-Flight-Code
  */
